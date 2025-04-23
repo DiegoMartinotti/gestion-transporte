58cfa509b1ebfc1e43e8394ef309ec6140c95329
DiegoMartinotti <diego270986@gmail.com>
2025-04-23 16:12:17 -0300

    refactor: Eliminar archivo JS obsoleto en favor de la versión TypeScript

--- backend/validators/viajeValidator.js
+++
@@ -1,3 +0,0 @@
-// Este archivo existe para mantener la compatibilidad durante la migración a TypeScript
-// Para proyectos nuevos, usa directamente el archivo .ts
-module.exports = require('../dist/validators/viajeValidator').default;
