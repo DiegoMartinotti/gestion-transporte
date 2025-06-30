eb23ccb882f173fbe7c3d86682267d9d9e3d51e8
DiegoMartinotti <diego270986@gmail.com>
2025-06-30 13:12:16 +0000

    feat: update routing and site management components for improved navigation and functionality

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,39 +1,52 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import { Container, Text } from '@mantine/core';
 import Dashboard from './pages/Dashboard';
 import ClientesPage from './pages/clientes/ClientesPage';
 import ClienteDetailPage from './pages/clientes/ClienteDetailPage';
+import ClienteFormPage from './pages/clientes/ClienteFormPage';
 import EmpresasPage from './pages/empresas/EmpresasPage';
 import EmpresaDetailPage from './pages/empresas/EmpresaDetailPage';
 import { PersonalPage } from './pages/personal/PersonalPage';
 import { SitesPage } from './pages/sites';
+import ProtectedRoute from './components/ProtectedRoute';
+import MainLayout from './components/layout/MainLayout';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
-export default function Router() {
+export default function AppRoutes() {
   return (
-    <BrowserRouter>
-      <Routes>
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route element={
+        <ProtectedRoute>
+          <MainLayout />
+        </ProtectedRoute>
+      }>
         <Route path="/" element={<Dashboard />} />
         
         {/* Clientes routes */}
         <Route path="/clientes" element={<ClientesPage />} />
+        <Route path="/clientes/new" element={<ClienteFormPage />} />
         <Route path="/clientes/:id" element={<ClienteDetailPage />} />
-        <Route path="/clientes/:id/edit" element={<div>Cliente Edit - Coming Soon</div>} />
-        <Route path="/clientes/:id/sites" element={<div>Cliente Sites - Coming Soon</div>} />
-        <Route path="/clientes/:id/tramos" element={<div>Cliente Tramos - Coming Soon</div>} />
+        <Route path="/clientes/:id/edit" element={<ClienteFormPage />} />
+        <Route path="/clientes/:clienteId/sites" element={<Container><Text>Sites del Cliente - Coming Soon</Text></Container>} />
+        <Route path="/clientes/:clienteId/tramos" element={<Container><Text>Tramos del Cliente - Coming Soon</Text></Container>} />
         
         {/* Empresas routes */}
         <Route path="/empresas" element={<EmpresasPage />} />
         <Route path="/empresas/:id" element={<EmpresaDetailPage />} />
-        <Route path="/empresas/:id/edit" element={<div>Empresa Edit - Coming Soon</div>} />
-        <Route path="/empresas/:id/personal" element={<div>Empresa Personal - Coming Soon</div>} />
-        <Route path="/empresas/:id/vehiculos" element={<div>Empresa Vehiculos - Coming Soon</div>} />
+        <Route path="/empresas/:id/edit" element={<Container><Text>Empresa Edit - Coming Soon</Text></Container>} />
+        <Route path="/empresas/:id/personal" element={<Container><Text>Personal de la Empresa - Coming Soon</Text></Container>} />
+        <Route path="/empresas/:id/vehiculos" element={<Container><Text>Vehículos de la Empresa - Coming Soon</Text></Container>} />
         
         {/* Other routes */}
         <Route path="/personal" element={<PersonalPage />} />
         <Route path="/sites" element={<SitesPage />} />
-        <Route path="/tramos" element={<div>Tramos - Coming Soon</div>} />
-        <Route path="/vehiculos" element={<div>Vehículos - Coming Soon</div>} />
-        <Route path="/viajes" element={<div>Viajes - Coming Soon</div>} />
-      </Routes>
-    </BrowserRouter>
+        <Route path="/tramos" element={<Container><Text>Tramos - Coming Soon</Text></Container>} />
+        <Route path="/vehiculos" element={<Container><Text>Vehículos - Coming Soon</Text></Container>} />
+        <Route path="/viajes" element={<Container><Text>Viajes - Coming Soon</Text></Container>} />
+      </Route>
+    </Routes>
   );
 }
